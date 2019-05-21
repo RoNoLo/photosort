830b98431dd8263f31607e1a41d09dc10708f03e
Ronald Locke <rl@markveys.com>
2019-05-21 15:37:42 +0200

    readded symfony/framework-bundle

--- src/Service/FixtureService.php
+++ src/Service/FixtureService.php
@@ -1,16 +1,9 @@
 <?php
 
-namespace App\Command;
+namespace App\Service;
 
-use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\ArrayInput;
-use Symfony\Component\Console\Input\InputArgument;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
-use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Filesystem\Exception\IOException;
 use Symfony\Component\Filesystem\Filesystem;
-use Symfony\Component\Finder\Finder;
 use Symfony\Component\Yaml\Yaml;
 
 class FixtureService
